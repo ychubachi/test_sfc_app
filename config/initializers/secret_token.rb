4b9450f5ebcf60d3bd667a658630ffddd361d429
Yoshihide Chubachi <yoshi@chubachi.net>
2013-09-27 08:34:05 +0000

    Generate a new Rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestSfcApp::Application.config.secret_key_base = 'febf3f1c76a7ced253a14d11c42e31e4c942feb36a31f64a6b0ef3d4ffe8a64ef361aded0ef81c5d54660e2fc83d807e12dd7942543168740bfd810c9909f917'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TestSfcApp::Application.config.session_store :cookie_store, key: '_test_sfc_app_session'
